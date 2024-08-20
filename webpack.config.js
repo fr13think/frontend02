0cf5ea3d90eed44fc31241afb06e82a8fa00cbb2
fr13think <fr13thinkpad@gmail.com>
2024-08-20 09:07:43 +0700

    webpack loaders plugin mode

---
+++ webpack.config.js
@@ -0,0 +1,24 @@
+const path = require('path');
+module.exports = {
+  mode: 'production',
+  entry: './src/index.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js',
+  },
+  module: {
+    rules: [
+      {
+        test: /\.css$/,
+        use: [
+          {
+            loader: 'style-loader',
+          },
+          {
+            loader: 'css-loader',
+          },
+        ],
+      },
+    ],
+  },
+};
