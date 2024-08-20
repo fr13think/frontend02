609310a959a5d5bc30029e1984ae6adbf89310a7
fr13think <fr13thinkpad@gmail.com>
2024-08-20 11:06:00 +0700

    .withapi

---
+++ src/index.js
@@ -0,0 +1,11 @@
+/*
+  PERINGATAN: Jangan ubah kode di bawah ini!
+*/
+
+import 'bootstrap/dist/css/bootstrap.min.css';
+import './styles/main.css';
+
+import 'regenerator-runtime';
+import main from './scripts/main';
+
+main();
---
+++ webpack.prod.js
@@ -0,0 +1,22 @@
+const { merge } = require('webpack-merge');
+const common = require('./webpack.common.js');
+
+module.exports = merge(common, {
+  mode: 'production',
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        use: [
+          {
+            loader: 'babel-loader',
+            exclude: /node_modules/,
+            options: {
+              presets: ['@babel/preset-env'],
+            },
+          },
+        ],
+      },
+    ],
+  },
+});
